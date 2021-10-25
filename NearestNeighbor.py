8052e02024e5dae03568ade66650ea46f12cc844
RushanWang1 <91592469+RushanWang1@users.noreply.github.com>
2021-10-25 17:31:03 +0200

    Add files via upload

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -26,12 +26,12 @@ def NearestNeighbor(input_signal, input_label):
     input_row, input_col = input_signal_cp.shape
     pixels_labels = np.zeros((input_row, input_col))
     background = input_signal_cp[input_label_cp == 0] 
-    background_center = np.sum(background)/len(background)
+    background_centertemp = np.sum(background)/len(background)
     trees = input_signal_cp[input_label_cp == 1] 
-    trees_center = np.sum(trees)/len(trees)
+    trees_centertemp = np.sum(trees)/len(trees)
     clouds = input_signal_cp[input_label_cp == 2] 
-    clouds_center = np.sum(clouds)/len(clouds)
-    return background_center,trees_center,clouds_center
+    clouds_centertemp = np.sum(clouds)/len(clouds)
+    return background_centertemp,trees_centertemp,clouds_centertemp
        
 if __name__ == "__main__":
     dataset = SatelliteSet(windowsize = 32,test=False)
@@ -43,16 +43,31 @@ if __name__ == "__main__":
     background_center = np.zeros(4);
     trees_center = np.zeros(4);
     clouds_center = np.zeros(4);
-    num = 1
+    num = 1;
+    numb = np.ones(4);
+    numt = np.ones(4);
+    numc = np.ones(4);
     for data in tqdm(dl):
         signal = 255*data[0][0];
         label = data[1][0];
         for i in range(0,4):
             background_center[i],trees_center[i],clouds_center[i] = NearestNeighbor(signal[i],label);
-            rbackground_center[i] = (background_center[i]+rbackground_center[i])/num;
-            rtrees_center[i] = (trees_center[i]+rtrees_center[i])/num;
-            rclouds_center[i] = (clouds_center[i]+rclouds_center[i])/num;
+            background_center = np.nan_to_num(background_center);
+            trees_center = np.nan_to_num(trees_center);
+            clouds_center = np.nan_to_num(clouds_center);
+            if (background_center[i] != 0 ):
+                rbackground_center[i] = (background_center[i]+rbackground_center[i])/2;
+                #numb[i] = numb[i]+1;
+            if (trees_center[i] != 0 ):
+                rtrees_center[i] = (trees_center[i]+rtrees_center[i])/2;
+                #numt[i] = numt[i]+1;
+            if (clouds_center[i] != 0 ):
+                rclouds_center[i] = (clouds_center[i]+rclouds_center[i])/2;
+                #numc[i] = numc[i]+1;
+            
             #print(background_center)            
-        print(rbackground_center)
+        #print(rbackground_center)
         num += 1;
+        #if num > 10000:
+            #exit()
         
